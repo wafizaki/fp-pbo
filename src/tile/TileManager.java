6376069d677010e62549ac4d69fe6d383fcdbaae
ardhikakrsh <ardhikakrishna@gmail.com>
2023-12-02 21:35:15 +0700

    new pixel and mapv1

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -20,7 +20,7 @@ public class TileManager {
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 
-		tile = new Tile[10];
+		tile = new Tile[50];
 		mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
 		getTileImage();
@@ -29,14 +29,54 @@ public class TileManager {
 	}
 
 	public void getTileImage() {
+		// placeholder
+		setup(0, "grass00", false);
+		setup(1, "grass00", false);
+		setup(2, "grass00", false);
+		setup(3, "grass00", false);
+		setup(4, "grass00", false);
+		setup(5, "grass00", false);
+		setup(6, "grass00", false);
+		setup(6, "grass00", false);
+		setup(7, "grass00", false);
+		setup(8, "grass00", false);
+		setup(9, "grass00", false);
+		// placeholder
 
-		setup(0, "grass01", false);
-		setup(1, "wall", true);
-		setup(2, "water", true);
-		setup(3, "earth", false);
-		setup(4, "palm2", true);
-		setup(5, "sand", false);
-		setup(6, "brokenTree1", true);
+		setup(10, "grass00", false);
+		setup(11, "grass01", false);
+		setup(12, "water00", true);
+		setup(13, "water01", true);
+		setup(14, "water02", true);
+		setup(15, "water03", true);
+		setup(16, "water04", true);
+		setup(17, "water05", true);
+		setup(18, "water06", true);
+		setup(19, "water07", true);
+		setup(20, "water08", true);
+		setup(21, "water09", true);
+		setup(22, "water10", true);
+		setup(23, "water11", true);
+		setup(24, "water12", true);
+		setup(25, "water13", true);
+		setup(26, "road00", false);
+		setup(27, "road01", false);
+		setup(28, "road02", false);
+		setup(29, "road03", false);
+		setup(30, "road04", false);
+		setup(31, "road05", false);
+		setup(32, "road06", false);
+		setup(33, "road07", false);
+		setup(34, "road08", false);
+		setup(35, "road09", false);
+		setup(36, "road10", false);
+		setup(37, "road11", false);
+		setup(38, "road12", false);
+		setup(39, "earth", false);
+		setup(40, "wall", true);
+		setup(41, "palm2", true);
+		setup(42, "hut", true);
+
 	}
 
 	public void setup(int index, String imageName, boolean collision) {
@@ -56,7 +96,7 @@ public class TileManager {
 
 	public void loadMap() {
 		try {
-			InputStream is = getClass().getResourceAsStream("/maps/world01.txt");
+			InputStream is = getClass().getResourceAsStream("/maps/worldV2.txt");
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 			int col = 0;
